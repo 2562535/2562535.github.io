8dbf8108b3c6d0c9fa75fa71c349a5895b2e74e6
A777 <66163490+2562535@users.noreply.github.com>
2022-12-03 19:47:28 +0800

    Update index.js

--- EatKun-main/static/index.js
+++ EatKun-main/static/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 30) return '鸡你太美';
-    if (score <= 60) return '鸡你太太美';
-    if (score <= 100) return '鸡鸡鸡鸡鸡';
-    if (score <= 150) return '唱 跳 rap 篮球';
-    return '你就是蔡徐坤本人？';
+    if (score <= 30) return 'WatchPaper太美(bushi';
+    if (score <= 60) return 'WatchPaper你叔叔美';
+    if (score <= 100) return '叔叔叔叔叔';
+    if (score <= 150) return 'J·K·罗琳与JK萝莉，不愧是叔叔！！！';
+    return '你就是WatchPaper本人？';
 }
 
 function toStr(obj) {
